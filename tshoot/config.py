ba266e4c91c8efc3c0b2c0d1fb69e4b3ecf5c587
Diego Sonaglia <diego.sonaglia@gmail.com>
2023-12-07 21:31:24 +0100

    Update config.py

--- tshoot/config.py
+++ tshoot/config.py
@@ -39,6 +39,7 @@ def load_settings(ask=None):
         envvar_prefix="TSHOOT",
         # root_path=os.path.dirname(__file__),
         settings_files=[SETTINGS_PATH],
+        DEFAULT_SETTINGS_PATH
     )
 
     required_settings_fields = [f["name"] for f in get_settings_fields()]
